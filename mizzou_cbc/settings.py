010bbafefabe98213dc363ad392d6232448d437e
smk37git <sdmain3@gmail.com>
2026-04-08 18:51:04 -0500

    Added CSRF trusted origins

--- mizzou_cbc/settings.py
+++ mizzou_cbc/settings.py
@@ -33,6 +33,8 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'claudebuilderclub.com,www.claudebuilderclub.com,localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://claudebuilderclub.com,https://www.claudebuilderclub.com').split(',')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
